4359f0cf6734b9e64a52f510b8efdf599f1b2c8c
Dave Henderson <dhenderson@gmail.com>
2024-03-18 12:27:47 -0400

    shut the linter up
    Signed-off-by: Dave Henderson <dhenderson@gmail.com>

--- codeowners.go
+++ codeowners.go
@@ -256,18 +256,22 @@ func getPattern(line string) (*regexp.Regexp, error) {
 	line = strings.ReplaceAll(line, magicStar, "*")
 
 	// Temporary regex
-	var expr = ""
-	if strings.HasSuffix(line, "/") {
+	expr := ""
+
+	switch {
+	case strings.HasSuffix(line, "/"):
 		expr = line + "(|.*)$"
-	} else if strings.HasSuffix(line, "/([^/]*)") {
+	case strings.HasSuffix(line, "/([^/]*)"):
 		expr = line + "$"
-	} else {
+	default:
 		expr = line + "($|/.+$)"
 	}
+
 	if strings.HasPrefix(expr, "/") {
 		expr = "^(|/)" + expr[1:]
 	} else {
 		expr = "^(|.*/)" + expr
 	}
+
 	return regexp.Compile(expr)
 }
